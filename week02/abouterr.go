95f85dae3f3ed4227c18e5091714759d0293f1b0
arvinho <254501375@qq.com>
2021-07-19 14:21:34 +0800

    update file name

--- week02/abouterr.go
+++ week02/abouterr.go
@@ -5,29 +5,30 @@
    解决办法:
    import "github.com/pkg/errors" 的Wrap方法对错误进行上下文包装，
    并携带原始错误信息，尽量保留完整的调用栈,方便调用者定位问题
- */
+*/
 package main
+
 import (
 	"database/sql"
 	"fmt"
 	"github.com/pkg/errors"
 )
 
-func GetSql() error{
-	return errors.Wrap(sql.ErrNoRows,"GetSql failed")
+func GetSql() error {
+	return errors.Wrap(sql.ErrNoRows, "GetSql failed")
 }
 
-func Call() error{
-	return errors.WithMessage(GetSql(),"Call failed")
+func Call() error {
+	return errors.WithMessage(GetSql(), "Call failed")
 	//return errors.WithStack(GetSql())
 }
 
-func main()  {
+func main() {
 	err := Call()
-	if err != nil{
+	if err != nil {
 		if errors.Cause(err) == sql.ErrNoRows {
-			fmt.Printf("data not found: %v\n",err)
-			fmt.Printf("%+v\n",err)
+			fmt.Printf("data not found: %v\n", err)
+			fmt.Printf("%+v\n", err)
 			return
 		}
 	}
